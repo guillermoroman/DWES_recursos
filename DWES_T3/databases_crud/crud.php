14804c92df88dccad4c8f054a218f05e9c585f34
guillermoroman <guillermoroman@outlook.com>
2023-11-21 12:08:22 +0100

    New files

--- DWES_T3/databases_crud/crud.php
+++ DWES_T3/databases_crud/crud.php
@@ -58,4 +58,41 @@ $insertar->execute();
 echo "<h1>Pizzas tras añadir nueva</h1>";
 listarPizzas($conn);
 
+//////////////////////////
+// MODIFICAR UNA PIZZA //
+//////////////////////////
 
+$nombrePizza = "Pizza modificada";
+$costePizza = 6.00;
+$precioPizza = 11.00;
+$ingredientesPizza = "Tomate, Mozzarella, Albahaca, Champiñones";
+
+$nombreOriginal = "Pizza prueba";
+
+ $modificar = $conn->prepare("UPDATE pizzas SET nombre = :nombre, coste = :coste, precio = :precio, ingredientes = :ingredientes WHERE nombre =:nombreOriginal");
+
+$modificar->bindParam(':nombre', $nombrePizza);
+$modificar->bindParam(':coste', $costePizza);
+$modificar->bindParam(':precio', $precioPizza);
+$modificar->bindParam(':ingredientes', $ingredientesPizza);
+
+$modificar->bindParam(':nombreOriginal', $nombreOriginal);
+
+$modificar->execute();
+
+echo "<h1>Pizzas tras modificar</h1>";
+listarPizzas($conn);
+
+//////////////////////
+// BORRAR UNA PIZZA //
+//////////////////////
+
+$nombreABorrar = "Pizza modificada";
+
+$eliminar = $conn->prepare("DELETE FROM pizzas WHERE nombre=:nombre");
+
+$eliminar->bindParam(':nombre', $nombreABorrar);
+$eliminar->execute();
+
+echo "<h1>Pizzas tras borrar</h1>";
+listarPizzas($conn);
